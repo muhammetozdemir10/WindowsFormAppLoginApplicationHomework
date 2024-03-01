7f9dfd89c9ee2e878124b7ea9c3d0d84d84455a4
muhammetozdemir10 <127602475+muhammetozdemir10@users.noreply.github.com>
2024-03-01 21:47:28 +0300

    Initial commit

---
+++ WindowsFormAppLoginApplicationHomework/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormAppLoginApplicationHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(294, 139);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(174, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(294, 91);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(174, 20);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(337, 209);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 67);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "button1";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ WindowsFormAppLoginApplicationHomework/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormAppLoginApplicationHomework
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * LOGIN UYGULAMASI
+         * 
+         * [TASARIM]
+         * - Bir tane yazi alani ekelenecek.
+         *  - yazi alanin adi (Name) usernameTextBox
+         *  
+         * - Bir tane yazi alani eklenecek.
+         *  - yazi alanin adi (Name) passwordTextBox
+         *  
+         * - Bir tane button eklenecek.
+         *  - Buttonun adi (Name) loginButton
+         *  - Buttonun click olayi olusturulacak.
+         * 
+         * [KOD]
+         * - Class scope'da sabit bir degisken tanimlanacak 
+         *  - Degiskenin adi Username
+         *  - Kullanici adini istedigin sekilde belirligebilirsin.
+         *  
+         * - Class scope'da sabit bir degisken tanimlanacak 
+         *  - Degiskenin adi Password
+         *  - Sifreyi istedigin sekilde belirligebilirsin.
+         *  
+         *  
+         * - Login butonun click fonksiyonunda login algoritmasi yazilacak.
+         * Login algoritmasinda kullanici adi ve sifre dogru oldugunda ekrana bir mesaj yazililacak.
+         * (MessageBox)
+         * 
+         * NOT: TextBox'larin icinde yazilan yazilara erismek icin 
+         * TextBox'in adini yazarak sonra .Text ozelligi ile icinde yazilan yaziyi ulasabilirsin.
+         *
+         * 
+         */
+        
+         const string Username = "Wizee";
+        const string Password = "Wizee6838";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (usernameTextBox.Text == Username && passwordTextBox.Text == Password)
+            {
+                MessageBox.Show("Giris Yapildi");
+            }else
+            {
+                if (usernameTextBox.Text != Username)
+                {
+                    MessageBox.Show("Kullanici adinizi kontrol ediniz");
+                    return;
+                }
+                
+
+                if (passwordTextBox.Text != Password)
+                {
+                    MessageBox.Show("Sifrenizi kontrol ediniz");
+                }
+            }
+        }
+        
+    }
+}
